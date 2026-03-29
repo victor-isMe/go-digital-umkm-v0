5e6f3d52d0da97df4d23ebfcf940436c77131efb
victor-isMe <muhammadfaizniwansyah23@gmail.com>
2026-03-29 13:49:30 +0700

    Nothing

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
+// let cart = JSON.parse(localStorage.getItem("cart")) || [];
+// updateCart();
+
 let jumlah = 1;
+
+//Untuk mangambil isi dari file header
+// fetch("template/header.php")
+//   .then(res => res.text())
+//   .then(data => {
+//     document.getElementById("header").innerHTML = data;
+//   });
+    
+// //Untuk mengambil isi dari file footer
+// fetch("template/footer.html")
+//   .then(res => res.text())
+//   .then(data => {
+//     document.getElementById("footer").innerHTML = data;
+//   });
+
+//Fungsi untuk fitur menampilkan menu navigasi pada tampilan mobile
+function toggleMenu() {
+  const menu = document.getElementById("navMenu");
+  menu.classList.toggle("active");
+}
 
 function tambah() {
   jumlah++;
