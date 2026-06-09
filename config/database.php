2c2e0dcdbee47cd9dbfeac63099ef7197397e6bd
victor-isMe <muhammadfaizniwansyah23@gmail.com>
2026-06-09 15:11:39 +0700

    Menambah koneksi database ke hosting

--- config/database.php
+++ config/database.php
@@ -1,8 +1,15 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pw = "";
-$db = "db_marketplace";
+if ($_SERVER['HTTP_HOST'] == 'localhost'){
+    $host = "localhost";
+    $user = "root";
+    $pw = "";
+    $db = "db_marketplace";
+} else {
+    $host = "sql105.infinityfree.com";
+    $user = "if0_42136918";
+    $pw = "kelompok7chill";
+    $db = "if0_42136918_db_marketplace";
+}
 
 $koneksi = mysqli_connect($host, $user, $pw);
 
