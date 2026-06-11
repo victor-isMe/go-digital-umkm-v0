22bf28035eb9b76c1881fda275ec754a0d68e8d7
victor-isMe <muhammadfaizniwansyah23@gmail.com>
2026-06-11 09:57:57 +0700

    Mengubah config database untuk keperluan hosting

--- config/database.php
+++ config/database.php
@@ -11,31 +11,7 @@ if ($_SERVER['HTTP_HOST'] == 'localhost'){
     $db = "if0_42136918_db_marketplace";
 }
 
-$koneksi = mysqli_connect($host, $user, $pw);
-
-$checkDB = mysqli_query($koneksi, "SHOW DATABASES LIKE '$db'");
-
-if (mysqli_num_rows($checkDB) == 0) {
-    mysqli_query($koneksi, "CREATE DATABASE $db");
-
-    mysqli_select_db($koneksi, $db);
-    
-    $sql = file_get_contents(__DIR__ . "/db_marketplace.sql");
-
-    if ($sql) {
-        if (mysqli_multi_query($koneksi, $sql)) {
-            do {
-                if ($result = mysqli_store_result($koneksi)) {
-                    mysqli_free_result($result);
-                }
-            } while (mysqli_next_result($koneksi));
-        } else {
-            echo "Error: " . mysqli_error($koneksi);
-        }
-    }
-} else {
-    mysqli_select_db($koneksi, $db);
-}
+$koneksi = mysqli_connect($host, $user, $pw, $db);
 
 if (mysqli_connect_errno()) {
     echo "Koneksi database gagal: " . mysqli_connect_error();
