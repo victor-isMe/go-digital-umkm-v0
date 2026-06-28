0b007995fae6107fb069cfe47d519254b9ed3c2d
victor-isMe <muhammadfaizniwansyah23@gmail.com>
2026-06-29 01:08:50 +0700

    Hapus navlink di footer

--- template/footer.php
+++ template/footer.php
@@ -9,15 +9,6 @@
             </div>
         </div>
 
-        <!-- LINKS -->
-        <div class="footer-links">
-            <a href="index.php?page=home">Home</a>
-            <a href="index.php?page=produk">Produk</a>
-            <?php if (!isset($_SESSION['login'])): ?>
-                <a href="index.php?page=login">Masuk</a>
-            <?php endif; ?>
-        </div>
-
         <!-- COPYRIGHT -->
         <div class="footer-copy">
             © <?= date('Y') ?> Go Digital UMKM. All rights reserved.
